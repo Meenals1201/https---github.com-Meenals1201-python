6c7c1ed44522778cd1ea4d989540cf245c89afb6
Meenals1201 <sameenal@sheridancollege.ca>
2025-11-24 15:11:44 -0500

    Created the basic routes to receive input via POST using form and various templates.

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-n
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/homepage")
+def homepage():
+    return render_template('homepage.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/registered", methods=['POST'])
+def registered():
+    if request.method == 'POST':
+
+        name = request.form.get('name')
+        email = request.form.get('email')
+
+        data = {
+          "first_name": name,
+          "email": email
+        }
+        
+    return render_template('registered.html', **data)
+
+
+
