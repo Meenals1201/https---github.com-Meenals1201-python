7d892b9fb1df77de8f56fea5c00dbb2e4fa2f5b4
Meenals1201 <sameenal@sheridancollege.ca>
2025-11-24 22:26:16 -0500

    Linked the database with the server, and typed a few input to store information in the database for signing up and for messaging.

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+import mysql.connector
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+
+conn = mysql.connector.connect(
+host=app.config['MYSQL_HOST'],
+user=app.config['MYSQL_USER'],
+password=app.config['MYSQL_PASSWORD'],
+database=app.config['MYSQL_DB']
+)
+
+
+cursor = conn.cursor()
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
 @app.route("/homepage")
 def homepage():
@@ -16,6 +38,12 @@ def registered():
 
         name = request.form.get('name')
         email = request.form.get('email')
+        password = request.form.get('password')
+
+        cursor.execute('''INSERT INTO userstable (name, email, password, role) 
+               VALUES (%s, %s, %s, %s)''', 
+               (name, email, password, 'member'))
+        conn.commit()
 
         data = {
           "first_name": name,
@@ -35,6 +63,11 @@ def message():
         name = request.form.get('name')
         email = request.form.get('email')
         message = request.form.get('text')
+
+        cursor.execute('''INSERT INTO contactstable (name, email, message) 
+                       VALUES (%s, %s, %s)''', 
+                       (name, email, message))
+        conn.commit()
 
         data = {
           "first_name": name,
